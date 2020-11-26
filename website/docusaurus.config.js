3bad9834c9192e4fc8f6cc53a4de06c060372b93
Romaric Philogene <evoxmusic@gmail.com>
2020-11-26 14:26:26 +0100

    update footer

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -33,7 +33,6 @@ module.exports = {
                 {to: 'docs/', label: 'Docs', position: 'left'},
                 {to: 'guides/', label: 'Guides', position: 'left'},
                 {to: 'guides/tutorial', label: 'Tutorials', position: 'left'},
-                {to: 'guides/engineering', label: 'Engineering', position: 'left'},
                 //{to: 'blog/', label: 'Blog', position: 'left'},
                 {to: 'community/', label: 'Community', position: 'right'},
                 // {to: 'releases/', label: 'Download', position: 'right'},

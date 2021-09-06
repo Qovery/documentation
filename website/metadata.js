88aeba0c9d194177c3528d3c83c6b36800f4d856
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-09-06 13:25:53 +0200

    feat: tutorial github orgs

--- website/metadata.js
+++ website/metadata.js
@@ -392,6 +392,15 @@ module.exports = {
           "title": "How to deploy a full-stack application composed of Hasura, PostgreSQL and Angular"
         },
         {
+          "author_github": "https://github.com/pjeziorowski",
+          "description": null,
+          "id": "/tutorial/github-organization-repository-access",
+          "last_modified_on": null,
+          "path": "website/guides/tutorial/github-organization-repository-access.md",
+          "series_position": null,
+          "title": "How to use Github Organizations with Qovery"
+        },
+        {
           "author_github": "https://github.com/MacLikorne",
           "description": null,
           "id": "/tutorial/how-to-write-a-dockerfile",

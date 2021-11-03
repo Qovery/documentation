8057f82831099f1d0810ca167bb1f8d427afc5c6
Pierre Mavro <pmavro@qovery.com>
2021-11-03 19:04:19 +0100

    feat: add live write for mac support (not tested)

--- scripts/live_update.sh
+++ scripts/live_update.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
+function check_bin() {
+    bin=$1
+    which $bin
+    status=$(echo $?)
+    if [ $status -ne 0 ] ; then
+        echo "$bin is not found, please install it"
+        exit 1
+    fi
+}
+
 echo "######################"
 echo "# START LIVE WRITING #"
 echo "######################"
 
+ORIG_PWD=$(pwd)
+cd website && yarn start &
+cd $ORIG_PWD
+
 if [ "$(uname)" == "Linux" ] ; then
-    ORIG_PWD=$(pwd)
-    cd website && yarn start &
-    cd $ORIG_PWD
+    check_bin inotifywait
     inotifywait -qrm -e close_write . | while read content ; do
         make generate USE_CONTAINER=none
     done
+elif [ "$(uname)" == "Darwin" ] ; then
+    check_bin fswatch
+    fswatch -o . | xargs -n1 -I{} make generate USE_CONTAINER=none
 else
-    echo "Sorry, your OS is not supported yet"
+    echo "Sorry, your OS is not supported"
 fi
 

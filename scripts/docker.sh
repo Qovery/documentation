62fe1e661f474041f9cb3c131effaa0f0f0528f5
Pierre Mavro <pmavro@qovery.com>
2022-11-07 09:08:30 +0100

    fix: adding missing docusaurus install process for docker run

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,5 +4,7 @@ export LANG=en_US.UTF-8
 # tmp fix https://stackoverflow.com/questions/69692842/error-message-error0308010cdigital-envelope-routinesunsupported
 export NODE_OPTIONS=--openssl-legacy-provider
 
-cd /mnt/documentation
+cd /mnt/documentation/website
+yarn
+cd ..
 ./scripts/live_update.sh
